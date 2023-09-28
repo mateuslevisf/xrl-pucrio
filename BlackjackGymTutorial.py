46fffdcb50796438b889417981662e7eb1948078
Mateus Levi <mateuslevisimoes@gmail.com>
2023-09-28 15:19:50 -0300

    :construction: Add training loop and plot functions

---
+++ BlackjackGymTutorial.py
@@ -0,0 +1,169 @@
+# External dependencies
+import numpy as np
+import gymnasium as gym
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from matplotlib.patches import Patch
+
+# Internal dependencies
+from TutorialBlackjackAgent import BlackjackAgent
+
+# Adapted from https://gymnasium.farama.org/tutorials/training_agents/blackjack_tutorial/#sphx-glr-tutorials-training-agents-blackjack-tutorial-py
+
+should_print = True
+
+def log(message):
+    if should_print:
+        print(message)
+
+# Make Blackjack environment.
+# 'sab' parameter defines environment following Sutton and Barton's book rules.
+env = gym.make("Blackjack-v1", sab=True)
+
+# We reset the environment. Done will be used to check if the game is over later;
+done = False
+observation, info = env.reset()
+
+# Observation follows the format: (player's current sum, dealer's face-up card, boolean whether the player has an usable ace)
+# Usable ace = an ace that can be used as 11 without the player going bust.
+log("Initial observation: {}".format(observation))
+
+# Hyperparameters definitions
+learning_rate = 0.01
+num_episodes = 100_000
+initial_epsilon = 1.0
+epsilon_decay = initial_epsilon / (num_episodes / 2)
+final_epsilon = 0.1
+
+agent = BlackjackAgent(learning_rate, initial_epsilon, epsilon_decay, final_epsilon, env.action_space)
+
+# Training loop
+env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=num_episodes)
+for episode in tqdm(range(num_episodes)):
+    obs, info = env.reset()
+    done = False
+
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # Update agent
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # Update environment status (done or not) and observation
+        done = terminated or truncated
+        obs = next_obs
+
+    agent.decay_epsilon()
+
+
+rolling_length = 500
+fig, axs = plt.subplots(ncols=3)
+axs[0].set_title("Episode rewards")
+# compute and assign a rolling average of the data to provide a smoother graph
+reward_moving_average = (
+    np.convolve(
+        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
+    )
+    / rolling_length
+)
+axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+axs[1].set_title("Episode lengths")
+length_moving_average = (
+    np.convolve(
+        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
+    )
+    / rolling_length
+)
+axs[1].plot(range(len(length_moving_average)), length_moving_average)
+axs[2].set_title("Training Error")
+training_error_moving_average = (
+    np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
+    / rolling_length
+)
+axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+plt.tight_layout()
+plt.show()
+
+def create_grids(agent, usable_ace=False):
+    """Create value and policy grid given an agent."""
+    # convert our state-action values to state values
+    # and build a policy dictionary that maps observations to actions
+    state_value = defaultdict(float)
+    policy = defaultdict(int)
+    for obs, action_values in agent.q_values.items():
+        state_value[obs] = float(np.max(action_values))
+        policy[obs] = int(np.argmax(action_values))
+
+    player_count, dealer_count = np.meshgrid(
+        # players count, dealers face-up card
+        np.arange(12, 22),
+        np.arange(1, 11),
+    )
+
+    # create the value grid for plotting
+    value = np.apply_along_axis(
+        lambda obs: state_value[(obs[0], obs[1], usable_ace)],
+        axis=2,
+        arr=np.dstack([player_count, dealer_count]),
+    )
+    value_grid = player_count, dealer_count, value
+
+    # create the policy grid for plotting
+    policy_grid = np.apply_along_axis(
+        lambda obs: policy[(obs[0], obs[1], usable_ace)],
+        axis=2,
+        arr=np.dstack([player_count, dealer_count]),
+    )
+    return value_grid, policy_grid
+
+def create_plots(value_grid, policy_grid, title: str):
+    """Creates a plot using a value and policy grid."""
+    # create a new figure with 2 subplots (left: state values, right: policy)
+    player_count, dealer_count, value = value_grid
+    fig = plt.figure(figsize=plt.figaspect(0.4))
+    fig.suptitle(title, fontsize=16)
+
+    # plot the state values
+    ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+    ax1.plot_surface(
+        player_count,
+        dealer_count,
+        value,
+        rstride=1,
+        cstride=1,
+        cmap="viridis",
+        edgecolor="none",
+    )
+    plt.xticks(range(12, 22), range(12, 22))
+    plt.yticks(range(1, 11), ["A"] + list(range(2, 11)))
+    ax1.set_title(f"State values: {title}")
+    ax1.set_xlabel("Player sum")
+    ax1.set_ylabel("Dealer showing")
+    ax1.zaxis.set_rotate_label(False)
+    ax1.set_zlabel("Value", fontsize=14, rotation=90)
+    ax1.view_init(20, 220)
+
+    # plot the policy
+    fig.add_subplot(1, 2, 2)
+    ax2 = sns.heatmap(policy_grid, linewidth=0, annot=True, cmap="Accent_r", cbar=False)
+    ax2.set_title(f"Policy: {title}")
+    ax2.set_xlabel("Player sum")
+    ax2.set_ylabel("Dealer showing")
+    ax2.set_xticklabels(range(12, 22))
+    ax2.set_yticklabels(["A"] + list(range(2, 11)), fontsize=12)
+
+    # add a legend
+    legend_elements = [
+        Patch(facecolor="lightgreen", edgecolor="black", label="Hit"),
+        Patch(facecolor="grey", edgecolor="black", label="Stick"),
+    ]
+    ax2.legend(handles=legend_elements, bbox_to_anchor=(1.3, 1))
+    return fig
+
+
+# state values & policy with usable ace (ace counts as 11)
+value_grid, policy_grid = create_grids(agent, usable_ace=True)
+fig1 = create_plots(value_grid, policy_grid, title="With usable ace")
+plt.show()
