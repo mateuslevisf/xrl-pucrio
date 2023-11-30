45a9479ce17dad2f8b1c7b6ae30f6ae7c27edbe9
Mateus <mateuslevisimoes@gmail.com>
2023-11-30 16:06:00 -0300

    :construction: Make hidden layer dimension an input of DQN

--- networks/dqn.py
+++ networks/dqn.py
@@ -3,14 +3,14 @@ import torch.nn.functional as F
 
 class DQN(nn.Module):
 
-    def __init__(self, n_observations, n_actions):
+    def __init__(self, n_observations, n_actions, hidden_dim=128):
         super(DQN, self).__init__()
         # Standard DQN architecture:
         # DQN takes the observations as input and outputs the Q-values for each action in the given state
         # 1 input layer, 1 hidden layer with 128 neurons and 1 output layer with n_actions neurons
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
+        self.layer1 = nn.Linear(n_observations, hidden_dim)
+        self.layer2 = nn.Linear(hidden_dim, hidden_dim)
+        self.layer3 = nn.Linear(hidden_dim, n_actions)
 
     def forward(self, x):
         # Called with either one element to determine next action, or a batch
